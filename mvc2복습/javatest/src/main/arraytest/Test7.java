172d367d4f0eb2ca778106f3718f8a4cd230ded2
pro <quddbscjswow@naver.com>
2022-05-10 01:00:27 +0900

    자바 기초 배열

--- mvc2복습/javatest/src/main/arraytest/Test7.java
+++ mvc2복습/javatest/src/main/arraytest/Test7.java
@@ -15,7 +15,6 @@ public class Test7 {
             int tmp = numArr[0];
             numArr[0] = numArr[n];
             numArr[n] = tmp;
-            System.out.println(numArr[9] + " = arrr");
         }
 
         for (int i = 0; i < numArr.length; i++) {
