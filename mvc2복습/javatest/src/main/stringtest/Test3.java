6b96c1a8f7c490cd391839092f90278cf16e1c31
pro <quddbscjswow@naver.com>
2022-05-12 01:03:35 +0900

    자바 기초 배열

---
+++ mvc2복습/javatest/src/main/stringtest/Test3.java
@@ -0,0 +1,22 @@
+package main.stringtest;
+
+public class Test3 {
+    public static void main(String[] args) {
+        String source = "SOSHELP";
+        String[] morse = {".-", "-...", "-.-.", "-..", "."
+                , "..-.", "--.", "....", "..", ".---"
+                , "-.-", ".-..", "--", "-.", "---"
+                , ".--", "--.-", ".-.", "...", "-"
+                , "..-", "...-", ".--", "-..-", "-.--"
+                , "--.."
+        };
+
+        String result = "";
+        System.out.println(source.charAt(1) - 'A');
+        for (int i = 0; i < source.length(); i++) {
+            result += morse[source.charAt(i) - 'A'];
+        }
+        System.out.println("source:" + source);
+        System.out.println("morse:" + result);
+    }
+}
